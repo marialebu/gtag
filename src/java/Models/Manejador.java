83a728d7891988957a438c95972b327355fc3524
Luis Felipe Díaz Chica <pipexir@gmail.com>
2016-04-23 14:42:11 -0500

    initial commit

---
+++ src/java/Models/Manejador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Felipe.Diaz
+ */
+public class Manejador {
+    
+    public ArrayList<Usuario> usuarios;
+    public ArrayList<Usuario> informacion;
+    public ArrayList<Usuario> categorias;
+    
+    
+}
---
+++ src/java/Models/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Felipe.Diaz
+ */
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
